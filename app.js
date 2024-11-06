27c1db2d1b8ae3cb37b6b8e27667d671262cf06a
gvaulttek <72730651+gvaulttek@users.noreply.github.com>
2024-11-06 15:51:38 +0400

    Add compression for transfer

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
+let compression = require('compression')
 let cors = require('cors');
 const app = express();
 app.use(cors());
+app.use(compression());
 const {allCombination} = require('./combinations');
-// const {project, menu, forbidden} = require('./data');
 app.use(express.json({limit:'200mb'}));
 
-app.put("/", function(req,res){
+app.post("/", function(req,res){
     if (!req.body) return res.sendStatus(400);
     let project = req.body.project;
     let menu = req.body.menu;
     let forbidden = req.body.forbs;
-    let data = allCombination(project,menu,forbidden)
+    let data = allCombination(project,menu,forbidden);
     res.send(data);
 });
 
